b03bab716c3427620dc7b9b8089181ea0af22a12
Mert Buran <Mert.Buran@monitise.com>
2015-03-22 01:45:40 +0200

    init commit

---
+++ RarJet/RarJet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RarJet
+//
+//  Created by Mert Buran on 22/03/15.
+//  Copyright (c) 2015 Mert Buran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RarJet/RarJet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RarJet
+//
+//  Created by Mert Buran on 22/03/15.
+//  Copyright (c) 2015 Mert Buran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
